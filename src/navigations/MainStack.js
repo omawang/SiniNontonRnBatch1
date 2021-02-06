1461a8bb837af56dccd789cfc1d5f3b4fec84a1f
Awank <mas.awank100@gmail.com>
2021-02-06 14:04:34 +0700

    setup bottom tab navigator

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../ui/screens/main/HomeScreen';
+import DummyScreen from '../ui/screens/main/DummyScreen';
 
 const Stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Dummy" component={DummyScreen} />
+      <Tab.Screen name="Dummy2" component={DummyScreen} />
+    </Tab.Navigator>
+  );
+}
 
 const MainStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={MyTabs} />
     </Stack.Navigator>
   );
 };
---
+++ src/ui/screens/main/DummyScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const DummyScreen = () => {
+  return (
+    <View>
+      <Text>Dummy</Text>
+    </View>
+  );
+};
+
+export default DummyScreen;
